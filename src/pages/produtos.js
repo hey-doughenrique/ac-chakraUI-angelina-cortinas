82eeb04f7e9e253cf40d34ffc1b9e5015dbb16c2
stridesdigital <douglas.henrique@strides.digital>
2022-05-12 16:44:54 -0300

    slugify

--- src/pages/produtos.js
+++ src/pages/produtos.js
@@ -1,9 +1,3 @@
-import { Box, Flex, Text } from "@chakra-ui/react";
-
-import Link from "next/link";
-import { Menu } from "../components/Header/index";
-import slugify from "slugify";
-
-export default function ProductsPage({ products }) {
+export default function ProductsPage() {
   return <h1> hello</h1>;
 }
