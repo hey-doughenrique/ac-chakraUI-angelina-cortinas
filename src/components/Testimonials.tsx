b02ec273b2d72b5ef76a17079643bf02caeecb42
stridesdigital <douglas.henrique@strides.digital>
2022-06-07 15:45:25 -0300

    testimonials updated

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -1,37 +1,54 @@
-import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Center,
+  Divider,
+  Flex,
+  HStack,
+  Stack,
+  Text,
+  VStack,
+} from "@chakra-ui/react";
 
-// const Depoimentos = {
-//   data: [
-//     {
-//       photo: "customer-1.png",
-//       name: "Douglas",
-//       content: "",
-//     },
-//     {
-//       photo: "",
-//       name: "Henrique",
-//       content: "",
-//     },
-//     {
-//       photo: "",
-//       name: "Jo'ao",
-//       content: "",
-//     },
-//   ],
-// };
+import Image from "next/image";
+import customerOne from "../../public/images/customer-1.png";
+import customerThree from "../../public/images/customer-3.png";
+import customerTwo from "../../public/images/customer-2.png";
+
+const Depoimentos = [
+  {
+    photo: customerOne,
+    name: "Samuel Santos",
+    content:
+      "Excelente! Desde o atendimento até o início da negociação. Recebi muitas sugestões, desde cores a valores. Entrega no prazo fornecido.",
+  },
+  {
+    photo: customerTwo,
+    name: "Marcelo Schwartz",
+    content:
+      "Fiquei impressionado com a atenção no atendimento e pontualidade na entrega e instalação. Muito satisfeito. Estão de parabéns",
+  },
+  {
+    photo: customerThree,
+    name: "Nathalia Guedes",
+    content:
+      "Ótimo atendimento, entrega no prazo, pessoas simpáticas e trabalho bem feito. Ficamos super contentes e repetiriamos a escolha. Parabéns :)",
+  },
+];
 
 export default function Testimonials() {
   return (
     <Box
+      className="Testimonials__Section"
       id="sobre"
       background={[
         "linear-gradient(4deg,#F7F6F3 46%,#ffffff 100%);",
-        "linear-gradient(180deg,#F7F6F3 48%,#ffffff 20%);",
+        "linear-gradient(180deg,#F7F6F3 34%,#ffffff 20%);",
       ]}
     >
       <Flex
         id="depoimentos"
-        justify="center"
+        className="Testimonials__Wrapper"
+        justifyContent="space-between"
         flexDirection={["column", "row"]}
         align="center"
         px="4"
@@ -39,26 +56,54 @@ export default function Testimonials() {
         mx="auto"
         maxW={["100%", "1080px"]}
       >
-        {/* {Depoimentos.data.map((client) => {
+        {Depoimentos.map((client) => {
           return (
             <>
-              <Flex py={["2", "0"]}>
-                <Image
+              <Stack
+                className="Testimonials__Item"
+                alignItems="flex-start"
+                flexDir={["row", "column"]}
+                flex="1"
+                pb={["40px", "0"]}
+              >
+                <Box
+                  position="relative"
+                  h="100px"
+                  w="100px"
                   borderRadius="full"
-                  boxSize="100px"
-                  src="/images/customer-1.png"
-                  alt="Foto de cliente da Angelina Cortinas"
-                />
-
-                <Box ml={4}>
-                  <Text as="h4">{client.name}</Text>
-                  <Text fontSize="sm">{client.content}</Text>
+                  outline="#ffffff solid 12px"
+                >
+                  <Image
+                    layout="fill"
+                    objectFit="contain"
+                    src={client.photo}
+                    alt={client.name}
+                  />
                 </Box>
-              </Flex>
+
+                <HStack
+                  className="Testimonials__Content"
+                  pl={["0", "48px"]} //48px
+                  spacing={7} // 28px
+                  flex="1"
+                >
+                  <Center h={["0", "140px"]}>
+                    <Divider orientation="vertical" />
+                  </Center>
+                  <VStack
+                    className="Testimonials__Details"
+                    alignItems="flex-start"
+                    gap={2} // 8px
+                  >
+                    <Text as="h4">{client.name}</Text>
+                    <Text fontSize="sm">{client.content}</Text>
+                  </VStack>
+                </HStack>
+              </Stack>
             </>
           );
-        })} */}
-        <Flex py={["2", "0"]}>
+        })}
+        {/* <Flex py={["2", "0"]}>
           <Image
             borderRadius="full"
             boxSize="100px"
@@ -105,7 +150,7 @@ export default function Testimonials() {
               Parabéns :)
             </Text>
           </Box>
-        </Flex>
+        </Flex> */}
       </Flex>
     </Box>
   );
